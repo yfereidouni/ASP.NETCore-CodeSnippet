2b754f2aab2001c044e2c64904c3f5251845fa51
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-06 22:26:13 +0430

    Errors handlers

---
+++ WebAPI/WebAPI/Controllers/GroceriesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroceriesController : ControllerBase
+    {
+        private readonly ShoppingListContext _context;
+
+        public GroceriesController(ShoppingListContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Groceries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Grocery>>> GetGroceries()
+        {
+          if (_context.Groceries == null)
+          {
+              return NotFound();
+          }
+            return await _context.Groceries.ToListAsync();
+        }
+
+        // GET: api/Groceries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Grocery>> GetGrocery(int id)
+        {
+          if (_context.Groceries == null)
+          {
+              return NotFound();
+          }
+            var grocery = await _context.Groceries.FindAsync(id);
+
+            if (grocery == null)
+            {
+                return NotFound();
+            }
+
+            return grocery;
+        }
+
+        // PUT: api/Groceries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGrocery(int id, Grocery grocery)
+        {
+            if (id != grocery.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(grocery).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GroceryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Groceries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Grocery>> PostGrocery(Grocery grocery)
+        {
+          if (_context.Groceries == null)
+          {
+              return Problem("Entity set 'ShoppingListContext.Groceries'  is null.");
+          }
+            _context.Groceries.Add(grocery);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetGrocery), new { id = grocery.Id }, grocery);
+        }
+
+        // DELETE: api/Groceries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGrocery(int id)
+        {
+            if (_context.Groceries == null)
+            {
+                return NotFound();
+            }
+            var grocery = await _context.Groceries.FindAsync(id);
+            if (grocery == null)
+            {
+                return NotFound();
+            }
+
+            _context.Groceries.Remove(grocery);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GroceryExists(int id)
+        {
+            return (_context.Groceries?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/ShoppingListContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models;
+
+public class ShoppingListContext : DbContext
+{
+    public DbSet<Grocery> Groceries { get; set; }
+    public ShoppingListContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
