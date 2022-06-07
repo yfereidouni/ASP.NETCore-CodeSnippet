5e30111826656d968a1771c7dc831ff18cacbe5a
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-07 10:24:34 +0430

    MinimalShoppingListAPI without controllers

---
+++ MinimalShoppingListAPI/MinimalShoppingListAPI/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalShoppingListAPI;
+
+public class ApiDbContext : DbContext
+{
+    public DbSet<Grocery> Groceries { get; set; }
+    //public DbSet<Grocery> Groceries => Set<Grocery>();
+ 
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
+    }
+}
