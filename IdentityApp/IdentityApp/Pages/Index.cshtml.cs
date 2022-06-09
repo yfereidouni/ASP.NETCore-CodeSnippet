f4a19814644133d62aff97304767791a06ad3d3f
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-09 19:08:56 +0430

    Chart.js and JsDeliver was added

--- IdentityApp/IdentityApp/Pages/Index.cshtml.cs
+++ IdentityApp/IdentityApp/Pages/Index.cshtml.cs
@@ -1,20 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityApp.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityApp.Pages
 {
     public class IndexModel : PageModel
     {
+        public Dictionary<string, int> Revenue;
+
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
+            Revenue = new Dictionary<string, int>()
+            {
+                {"January",0 },
+                {"February",0},
+                {"March",0},
+                {"April",0 },
+                {"May",0 },
+                {"June",0 },
+                {"July",0 },
+                {"August",0 },
+                {"September",0 },
+                {"October",0 },
+                {"November",0 },
+                {"December",0 },
+            };
 
+            var invoices = _context.Invoices.ToList();
+            foreach (var invoice in invoices)
+            {
+                Revenue[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+            }
         }
     }
 }
