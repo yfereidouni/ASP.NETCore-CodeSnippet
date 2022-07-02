bb2ddcf9ea102c248729790af0ca0ec67c97bd90
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-07-02 12:12:10 +0430

    CRUD on movies

--- iIdentity/iUdemy.Secure.NET.Microservices.IdentityServer4/Movies.API/Controllers/MoviesController.cs
+++ iIdentity/iUdemy.Secure.NET.Microservices.IdentityServer4/Movies.API/Controllers/MoviesController.cs
@@ -29,5 +29,92 @@ namespace Movies.API.Controllers
         {
             return await moviesAPIContext.Movies.FindAsync(id);
         }
+
+        [HttpPost("Create")]
+        public async Task<Movie> Create([FromBody] Movie model)
+        {
+            var movieInDb = await moviesAPIContext.Movies.FindAsync(model.Id);
+
+            if (movieInDb == null)
+            {
+                Movie movie = new()
+                {
+                    Id = model.Id,
+                    Title = model.Title,
+                    Genre = model.Genre,
+                    ImageUrl = model.ImageUrl,
+                    Owner = model.Owner,
+                    Rating = model.Rating,
+                    ReleaseDate = model.ReleaseDate
+                };
+                await moviesAPIContext.AddAsync(movie);
+                await moviesAPIContext.SaveChangesAsync();
+                return movie;
+            }
+            else
+            {
+                /// Create new Movie if ID exist in DB
+                Movie movie = new()
+                {
+                    Title = model.Title,
+                    Genre = model.Genre,
+                    ImageUrl = model.ImageUrl,
+                    Owner = model.Owner,
+                    Rating = model.Rating,
+                    ReleaseDate = model.ReleaseDate
+                };
+                await moviesAPIContext.AddAsync(movie);
+                await moviesAPIContext.SaveChangesAsync();
+                return movie;
+
+                ///// Update Movie if ID exist in DB
+                //movieInDb.Id = model.Id;
+                //movieInDb.Title = model.Title;
+                //movieInDb.Genre = model.Genre;
+                //movieInDb.ImageUrl = model.ImageUrl;
+                //movieInDb.Owner = model.Owner;
+                //movieInDb.Rating = model.Rating;
+                //movieInDb.ReleaseDate = model.ReleaseDate;
+
+                //await moviesAPIContext.AddAsync(movieInDb);
+                //await moviesAPIContext.SaveChangesAsync();
+            }
+        }
+
+        [HttpPut("Update")]
+        public async Task<Movie> Update([FromBody] Movie model)
+        {
+            var movieInDb = await moviesAPIContext.Movies.FindAsync(model.Id);
+
+            if (movieInDb != null)
+            {
+                movieInDb.Id = model.Id;
+                movieInDb.Title = model.Title;
+                movieInDb.Genre = model.Genre;
+                movieInDb.ImageUrl = model.ImageUrl;
+                movieInDb.Owner = model.Owner;
+                movieInDb.Rating = model.Rating;
+                movieInDb.ReleaseDate = model.ReleaseDate;
+
+                //await moviesAPIContext.AddAsync(movieInDb);
+                await moviesAPIContext.SaveChangesAsync();
+            }
+
+            return movieInDb;
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<Movie> Delete([FromQuery] int id)
+        {
+            var movieInDb = await moviesAPIContext.Movies.FindAsync(id);
+
+            if (movieInDb != null)
+            {
+                moviesAPIContext.Movies.Remove(movieInDb);
+                await moviesAPIContext.SaveChangesAsync();
+            }
+
+            return movieInDb;
+        }
     }
 }
