5c732d3512e8bdbf14bda18434624593a888b96a
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-07-08 23:03:12 +0430

    ASP.NET Core MVC sample app was developed.

---
+++ Udemy.ASP.NETCore6.MVC.Blazor.Razor.EF/S02E04.FirstMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace S02E04.FirstMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
