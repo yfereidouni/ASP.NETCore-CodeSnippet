474e61ef76a3f06f947f88f0e2de90a8bbaf3d7f
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-07-08 06:27:11 +0430

    GetUserInfo() was developed.

--- iIdentity/iUdemy.Secure.NET.Microservices.IdentityServer4/Movies.Client/ApiServices/MovieApiService.cs
+++ iIdentity/iUdemy.Secure.NET.Microservices.IdentityServer4/Movies.Client/ApiServices/MovieApiService.cs
@@ -1,4 +1,6 @@
 ﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Movies.Client.Models;
 using Newtonsoft.Json;
 using System.Net.Http;
@@ -8,12 +10,12 @@ namespace Movies.Client.ApiServices
     public class MovieApiService : IMovieApiService
     {
         private readonly IHttpClientFactory _httpClientFactory;
-
-        public MovieApiService(IHttpClientFactory httpClientFactory)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public MovieApiService(IHttpClientFactory httpClientFactory,IHttpContextAccessor httpContextAccessor)
         {
             _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            _httpContextAccessor = httpContextAccessor;
         }
-
         public async Task<IEnumerable<Movie>> GetMovies()
         {
             // Way 1
@@ -89,25 +91,56 @@ namespace Movies.Client.ApiServices
             //return movieList;
             #endregion
         }
-
         public Task<Movie> GetMovie(int id)
         {
             throw new NotImplementedException();
         }
-
         public Task<Movie> CreateMovie(Movie movie)
         {
             throw new NotImplementedException();
         }
-
         public Task DeleteMovie(int id)
         {
             throw new NotImplementedException();
         }
-
         public Task<Movie> UpdateMovie(Movie movie)
         {
             throw new NotImplementedException();
+        }
+        public async Task<UserInfoViewModel> GetUserInfo()
+        {
+            var idpClient = _httpClientFactory.CreateClient("IDPClient");
+
+            var metaDataResponse = await idpClient.GetDiscoveryDocumentAsync();
+
+            if (metaDataResponse.IsError)
+            {
+                throw new HttpRequestException("Something went wrong while requesting the access token");
+            }
+
+            var accessToken = await _httpContextAccessor
+                .HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            var userInfoResponse = await idpClient.GetUserInfoAsync(
+                new UserInfoRequest
+                {
+                    Address = metaDataResponse.UserInfoEndpoint,
+                    Token = accessToken
+                });
+
+            if (userInfoResponse.IsError)
+            {
+                throw new HttpRequestException("Something went wrong while getting user info");
+            }
+
+            var userInfoDirectory = new Dictionary<string, string>();
+
+            foreach (var claim in userInfoResponse.Claims)
+            {
+                userInfoDirectory.Add(claim.Type, claim.Value);
+            }
+
+            return new UserInfoViewModel(userInfoDirectory);
         }
     }
 }
