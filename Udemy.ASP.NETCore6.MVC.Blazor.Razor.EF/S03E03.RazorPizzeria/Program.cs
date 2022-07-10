63d61b2a6d0f9a5991868f2485b85824c285c34e
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-07-10 16:22:42 +0430

    Selecting InMemory DB on startup.

--- Udemy.ASP.NETCore6.MVC.Blazor.Razor.EF/S03E03.RazorPizzeria/Program.cs
+++ Udemy.ASP.NETCore6.MVC.Blazor.Razor.EF/S03E03.RazorPizzeria/Program.cs
@@ -1,31 +1,47 @@
 using RazorPizzeria.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+/// Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+var IsInMemroyUsed = true;
+if (IsInMemroyUsed)
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+    /// Use InMemory DB ----------------------------------------------------
+    builder.Services.AddDbContext<ApplicationDbContext>(c =>
+        c.UseInMemoryDatabase("S03E03.RazorPizzeria_InMemoryDB"));
+    ///---------------------------------------------------------------------
+}
+else
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+}
+
 
 var app = builder.Build();
 
-// Creating DB at runtime with performing migration
-using (var scope = app.Services.CreateScope())
+if (!IsInMemroyUsed)
 {
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    context.Database.Migrate();
+    /// Creating DB at runtime with performing migration
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
 }
 
-// Configure the HTTP request pipeline.
+/// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    /// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
