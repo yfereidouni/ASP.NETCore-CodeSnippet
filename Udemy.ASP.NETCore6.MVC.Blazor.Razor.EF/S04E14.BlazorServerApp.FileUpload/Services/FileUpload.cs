06c66384ca86cb1c705eac2e839a39b03d54d155
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-07-13 11:37:34 +0430

    File Upload in Blazor

---
+++ Udemy.ASP.NETCore6.MVC.Blazor.Razor.EF/S04E14.BlazorServerApp.FileUpload/Services/FileUpload.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace S04E14.BlazorServerApp.FileUpload.Services;
+
+public interface IFileUpload
+{
+    Task UploadFile(IBrowserFile file);
+}
+
+public class FileUpload : IFileUpload
+{
+    private IWebHostEnvironment _webHostEnvironment;
+    private readonly ILogger<FileUpload> _logger;
+
+    public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+    {
+        _webHostEnvironment = webHostEnvironment;
+        _logger = logger;
+    }
+
+    public async Task UploadFile(IBrowserFile file)
+    {
+        if (file is not null)
+        {
+            try
+            {
+                var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads", file.Name);
+                using (var stream = file.OpenReadStream())
+                {
+                    var fileStream = File.Create(uploadPath);
+                    await stream.CopyToAsync(fileStream);
+                    fileStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+            }
+        }
+    }
+}
