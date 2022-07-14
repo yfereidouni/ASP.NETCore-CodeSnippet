249818ba0ed094afae81064b51c84e8fd5521566
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-07-15 00:14:49 +0430

    S06E03.ShoppingListAPI was developed.

---
+++ Udemy.ASP.NETCore6.MVC.Blazor.Razor.EF/S06E03.ShoppingListAPI/Models/ShoppingListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace S06E03.ShoppingListAPI.Models;
+
+public class ShoppingListContext : DbContext
+{
+    public DbSet<Grocery> Groceries { get; set; }
+
+    public ShoppingListContext(DbContextOptions<ShoppingListContext> options)
+        : base(options)
+    {
+    }
+}
