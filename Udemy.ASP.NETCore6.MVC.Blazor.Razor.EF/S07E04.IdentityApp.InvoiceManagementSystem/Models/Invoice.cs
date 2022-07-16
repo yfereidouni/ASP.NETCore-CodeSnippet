6943c845f91aeabed0e6f19492e22b2495f7e8e8
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-07-16 23:46:07 +0430

    Authorization was implimented.

--- Udemy.ASP.NETCore6.MVC.Blazor.Razor.EF/S07E04.IdentityApp.InvoiceManagementSystem/Models/Invoice.cs
+++ Udemy.ASP.NETCore6.MVC.Blazor.Razor.EF/S07E04.IdentityApp.InvoiceManagementSystem/Models/Invoice.cs
@@ -6,13 +6,13 @@ public class Invoice
     public double InvoiceAmount { get; set; }
     public string InvoiceMonth { get; set; }
     public string InvoiceOwner { get; set; }
-    //public string CreatorId { get; set; }
-    //public InvoiceStatus Status { get; set; }
+    public string CreatorId { get; set; }
+    public InvoiceStatus Status { get; set; }
 }
 
-//public enum InvoiceStatus
-//{
-//    Submitted,
-//    Approved,
-//    Rejected
-//}
+public enum InvoiceStatus
+{
+    Submitted,
+    Approved,
+    Rejected
+}
